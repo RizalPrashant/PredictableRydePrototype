c6640375e58857b7265cd180c7e633d69fff260d
Prashant Rizal <Prashant@Prashants-MacBook-Pro.local>
2019-02-11 15:25:43 -0700

    Finished putting Mapkit Pins and drawing a route

--- PredictableRydePrototype/ViewController.swift
+++ PredictableRydePrototype/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class customPin: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(pinTitle: String, pinSubtitle:String, location:CLLocationCoordinate2D){
+        self.title = pinTitle
+        self.subtitle = pinSubtitle
+        self.coordinate = location
+    }
+}
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude: 43.615021, longitude: -116.202316)
+        let destinationLocation = CLLocationCoordinate2D(latitude: 43.585320, longitude: -116.573578)
+        
+        let sourcePin = customPin(pinTitle: "Boise", pinSubtitle: "", location: sourceLocation)
+        
+        let destinationPin = customPin(pinTitle: "Nampa", pinSubtitle: "", location: destinationLocation)
+        self.mapView.addAnnotation(sourcePin)
+        self.mapView.addAnnotation(destinationPin)
+        
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
+        
+        let directionRequest = MKDirections.Request()
+        
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+        
+        directionRequest.transportType = .automobile
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let directionResponse = response else{
+                if let error = error{
+                    print("error getting directions")
+                }
+                return
+            }
+            
+            let route = directionResponse.routes[0]
+            self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+            
+        }
+        
+        self.mapView.delegate = self
     }
-
-
+    
+    //MARK:= MapKit delegates
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 4.0
+        return renderer
+    }
 }
 
