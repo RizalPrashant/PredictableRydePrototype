cd4d71a004107b52bbb0600fb7a6d40545f5d6f5
Prashant Rizal <Prashant@prashants-macbook-pro.local>
2019-02-10 22:47:02 -0700

    Initial Commit

---
+++ PredictableRydePrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PredictableRydePrototype
+//
+//  Created by Prashant Rizal on 2/10/19.
+//  Copyright © 2019 Prashant Rizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
